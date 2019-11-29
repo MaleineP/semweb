be8b11d87e4c7fbb641cec5e8713c3e8b4a8ffaf
MaleineP <sarah.laurax@gmail.com>
2019-11-29 16:38:04 +0100

    travail du 29/11

---
+++ bicycle_location/src/main/java/fr/emse/master/bicycle_location/MainController.java
@@ -0,0 +1,16 @@
+package fr.emse.master.bicycle_location;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MainController
+{
+	@RequestMapping(value="/", method = RequestMethod.GET)
+    public String gotToIndex(Model m) {
+        return "index.html";
+    }
+	
+}
