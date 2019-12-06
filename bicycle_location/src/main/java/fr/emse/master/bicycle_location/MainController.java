731e2cdce9f6ec1c24dcdf2d4d7d9364081d1445
MaleineP <sarah.laurax@gmail.com>
2019-12-06 16:49:57 +0100

    travail du 06/12 bis

--- bicycle_location/src/main/java/fr/emse/master/bicycle_location/MainController.java
+++ bicycle_location/src/main/java/fr/emse/master/bicycle_location/MainController.java
@@ -2,6 +2,7 @@ package fr.emse.master.bicycle_location;
 
 import java.io.File;
 
+import org.apache.jena.rdf.model.ModelFactory;
 import org.semanticweb.owlapi.apibinding.OWLManager;
 import org.semanticweb.owlapi.model.OWLException;
 import org.semanticweb.owlapi.model.OWLOntology;
@@ -16,13 +17,14 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class MainController {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String gotToIndex(Model m) {
-		
 		/*try {
 			OWLOntology ontology = this.loadOntology();
 		} catch (OWLException e) {
 			e.printStackTrace();
 		}*/
-
+		org.apache.jena.rdf.model.Model model = ModelFactory.createDefaultModel();
+		model.read("document.ttl") ;
+		System.out.println(model.toString());
 		m.addAttribute("saint_etienne", null);//rajouter l'objet contenant toutes les informations du Turtle
 		
 		return "index.html";
