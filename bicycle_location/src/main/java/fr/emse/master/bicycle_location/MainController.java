bafe7d05195939f0983c3145f6913983ccc32c64
MaleineP <sarah.laurax@gmail.com>
2019-12-11 12:37:11 +0100

    travail du 11/12

--- bicycle_location/src/main/java/fr/emse/master/bicycle_location/MainController.java
+++ bicycle_location/src/main/java/fr/emse/master/bicycle_location/MainController.java
@@ -1,8 +1,18 @@
 package fr.emse.master.bicycle_location;
 
 import java.io.File;
+import java.util.ArrayList;
 
 import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.Property;
+import org.apache.jena.rdf.model.RDFNode;
+import org.apache.jena.rdf.model.ResIterator;
+import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.rdf.model.Selector;
+import org.apache.jena.rdf.model.SimpleSelector;
+import org.apache.jena.rdf.model.Statement;
+import org.apache.jena.rdf.model.StmtIterator;
+import org.apache.jena.reasoner.rulesys.impl.SafeTripleIterator;
 import org.semanticweb.owlapi.apibinding.OWLManager;
 import org.semanticweb.owlapi.model.OWLException;
 import org.semanticweb.owlapi.model.OWLOntology;
@@ -17,33 +27,36 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class MainController {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String gotToIndex(Model m) {
-		/*try {
-			OWLOntology ontology = this.loadOntology();
-		} catch (OWLException e) {
-			e.printStackTrace();
-		}*/
-		org.apache.jena.rdf.model.Model model = ModelFactory.createDefaultModel();
-		model.read("document.ttl") ;
-		System.out.println(model.toString());
-		m.addAttribute("saint_etienne", null);//rajouter l'objet contenant toutes les informations du Turtle
+		/*
+		 * try { OWLOntology ontology = this.loadOntology(); } catch (OWLException e) {
+		 * e.printStackTrace(); }
+		 */
+		org.apache.jena.rdf.model.Model model_static = ModelFactory.createDefaultModel();
+		org.apache.jena.rdf.model.Model model_dynamic = ModelFactory.createDefaultModel();
+
+		model_static.read("ttl_static.ttl");
+		model_dynamic.read("ttl_dynamic.ttl");
+
+		StmtIterator liste_dynamic = model_dynamic.listStatements();
+		StmtIterator liste_static = model_static.listStatements();
 		
+		m.addAttribute("saint_etienne_static", liste_static);
+		m.addAttribute("saint_etienne_dynamic", liste_dynamic);
 		return "index.html";
 	}
 
-	/*public OWLOntology loadOntology() throws OWLException {
-
-		OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
-		OWLOntology localOntology = null;
-
-		File file = new File("ontology_bicycle_sharing.owl");
-
-		// loading the ontology
-		try {
-			localOntology = manager.loadOntologyFromOntologyDocument(file);
-		} catch (OWLOntologyCreationException e) {
-			e.printStackTrace();
-		}
-		return localOntology;
-	}*/
+	/*
+	 * public OWLOntology loadOntology() throws OWLException {
+	 * 
+	 * OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
+	 * OWLOntology localOntology = null;
+	 * 
+	 * File file = new File("ontology_bicycle_sharing.owl");
+	 * 
+	 * // loading the ontology try { localOntology =
+	 * manager.loadOntologyFromOntologyDocument(file); } catch
+	 * (OWLOntologyCreationException e) { e.printStackTrace(); } return
+	 * localOntology; }
+	 */
 
 }
